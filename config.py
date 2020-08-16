f52cd0c0d1ac068f63f4fc7895cad61a8abdd0da
itscomputers <its_computers@protonmail.com>
2020-08-16 15:06:36 -0600

    oh hi

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'its-a-history-report-not-a-babe-report'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'api.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
